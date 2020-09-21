8698c4faa6a3b712c4091fde8c18723b361467ac
Luis Hercules <ilegra.hercules@agibank.com.br>
2020-09-21 13:25:07 -0300

    Update dev

--- vesecresce/js/custom.js
+++ vesecresce/js/custom.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
     $('#home').parallax("100%", 0.1);
     // $('#overview').parallax("100%", 0.3);
     $('#trainer').parallax("100%", 0.2);
-    $('#newsletter').parallax("100%", 0.3);
+    // $('#newsletter').parallax("100%", 0.3);
     $('#blog').parallax("100%", 0.1);
     $('#price').parallax("100%", 0.2);
     $('#testimonial').parallax("100%", 0.2);
