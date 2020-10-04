00e5fc2bed0f9f57dd7d9153fa8ed48e9a885329
Tony <tonyleiva@hotmail.com>
2020-10-04 19:57:26 -0300

    Update goToProduct function

--- vesecresce/js/functions.js
+++ vesecresce/js/functions.js
@@ -1,14 +1,23 @@
 function goToProduct() {
 	trackingClick(1)
-	//window.open("https://www.hotmart.com/product/programa-ve-se-cresce/H41183358H", "_blank")
-	
-	$('html, body').stop().animate({
-		scrollTop: $("#newsletter").offset().top - 49
-	}, 1000)
+
+	var launchDatetime = getProductLaunchDatetime()
+	var now = new Date().getTime()
+	if (launchDatetime > now) {
+		$('html, body').stop().animate({
+			scrollTop: $("#newsletter").offset().top - 49
+		}, 1000)
+	} else {
+		window.open("https://www.hotmart.com/product/programa-ve-se-cresce/H41183358H", "_blank")
+	}
 }
 
 function isMobileBrowser() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+}
+
+function getProductLaunchDatetime() {
+	return new Date(2020, 10 - 1, 8, 21, 00, 00).getTime()
 }
 
 function copyToClipboard(elementId) {
