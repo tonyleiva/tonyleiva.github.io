c423ab338002ac7e460b69ca31071fa2fc50274a
Tony <tonyleiva@hotmail.com>
2020-10-04 20:14:37 -0300

    Update dev

--- vesecresce/js/custom-dev.js
+++ vesecresce/js/custom-dev.js
@@ -131,7 +131,7 @@ $(document).ready(function () {
   new WOW({ mobile: false }).init();
 
   /* countdown */
-  var countDownDate = new Date(2020, 10 - 1, 8, 21, 00, 00).getTime();
+  var countDownDate = getProductLaunchDatetime()
   // Update the count down every 1 second
   var x = setInterval(() => {
     // Get today's date and time
@@ -143,15 +143,20 @@ $(document).ready(function () {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Display the result in the element with id="demo"
-    document.getElementById("cdDays").innerHTML = days
-    document.getElementById("cdHours").innerHTML = hours
-    document.getElementById("cdMinutes").innerHTML = minutes
-    document.getElementById("cdSeconds").innerHTML = seconds
+    
     // If the count down is finished, write some text
-    if (distance < 0) {
+    if (distance > 0) {
+      // Display the result in the element with id="demo"
+      document.getElementById("cdDays").innerHTML = days
+      document.getElementById("cdHours").innerHTML = hours
+      document.getElementById("cdMinutes").innerHTML = minutes
+      document.getElementById("cdSeconds").innerHTML = seconds
+    } else {
       clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
+      document.getElementById("cdDays").innerHTML = 0
+      document.getElementById("cdHours").innerHTML = 0
+      document.getElementById("cdMinutes").innerHTML = 0
+      document.getElementById("cdSeconds").innerHTML = 0
     }
   }, 1000);
 });
